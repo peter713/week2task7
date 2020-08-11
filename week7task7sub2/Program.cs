d23251527faf5af4e76654b2e051d0fc860d76cb
Peter713 <plaszuk@onet.eu>
2020-08-11 22:29:30 +0200

    Second commit weel2task7sub2

---
+++ week7task7sub2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace week2task7sub2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//check if entered number is odd or even
+
+            Console.WriteLine("If you want to check, if number is even or odd, please type in this number");
+            string myString = Console.ReadLine();
+
+            int myNumber;
+            bool isNumerical = int.TryParse(myString, out myNumber);
+
+            if (isNumerical)
+            {
+                if (myNumber % 2 == 0)
+                {
+                    Console.WriteLine($"{myNumber} is even");
+                }
+                else
+                {
+                    Console.WriteLine($"{myNumber} is odd");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No possibility to check if this data is even or odd, please launch app again and type in correct data.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
