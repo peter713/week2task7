d7ecdb7c42c426354ca2f2938ccfc126cf660930
Peter713 <plaszuk@onet.eu>
2020-08-11 23:33:29 +0200

    fifth commit week2task7sub4

---
+++ week2task7sub4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace week2task7sub4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//type in year I will answer you, if it is leap year
+
+            Console.WriteLine("Type in year I will answer you, if it is leap year");
+
+            string strYear = Console.ReadLine();
+            bool isNum = int.TryParse(strYear, out int intYear);
+
+            if (isNum && intYear >= 0)
+            {
+                if (intYear % 4 == 0)
+                {
+                    Console.WriteLine($"{intYear} is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine($"{intYear} isn't a leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong, please try again.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
