c7ebebc881ba32a9f849fc72da8653787dd5d144
Peter713 <plaszuk@onet.eu>
2020-08-11 23:11:08 +0200

    fourth commit week2task7sub3

---
+++ week2task7sub3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace week2task7sub3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//check if number is + or -
+
+            double myNumber;
+
+            Console.WriteLine("Enter number, please");
+            myNumber = double.Parse(Console.ReadLine());
+
+            if (myNumber < 0)
+            {
+                Console.WriteLine($"{myNumber} is minus number");
+            }
+            else if (myNumber > 0)
+            {
+                Console.WriteLine($"{myNumber} is plus number");
+            }
+            else
+            {
+                Console.WriteLine($"{myNumber} is zero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
