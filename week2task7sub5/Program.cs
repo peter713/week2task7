c88ac89abf008f0824c3e052da8b2145e323b339
Peter713 <plaszuk@onet.eu>
2020-08-12 21:35:56 +0200

    sixth commit week2task7sub5

---
+++ week2task7sub5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace week2task7sub5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//check if age type in by user permit to canditate on presindency, prime minister, senator, member of parliament (represenative)
+            string strAge;
+            byte age;
+            Console.WriteLine("Please type in your age or other example age");
+            strAge = Console.ReadLine();
+            bool isAge = byte.TryParse(strAge, out age);
+
+            if (isAge)
+            {
+                if (age >= 21 && age < 30)
+                {
+                    Console.WriteLine("Your age gives you warranty to be canditate of the member of parliament or even prime minister");
+                }
+                else if (age >= 21 && age < 35)
+                {
+                    Console.WriteLine("Your age gives you warranty to be canditate of the member of parliament, prime minister or senator");
+                }
+                else if (age >= 35)
+                {
+                    Console.WriteLine("Your age gives you warranty to be canditate of the member of parliament, prime minister, senator or president");
+                }
+                else
+                {
+                    Console.WriteLine("Your age doesn't give you warranty to be canditate of the member of parliament, prime minister, senator or president");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your input is incorrect, please try again with proper data");
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
